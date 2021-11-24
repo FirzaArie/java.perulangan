c00481798cf2415daaad29911a0a673d2eb68bad
Grotleuser <Grotleuser@192.168.0.4>
2021-11-24 10:26:47 +0700

    Testing

---
+++ src/com/perulangan/main/Perulangan.java
@@ -0,0 +1,12 @@
+package com.perulangan.main;
+
+public class Perulangan {
+public static void main(String[] args) {
+	
+	int n = 8;
+	for(int i=1; i<=10; i++) {
+		System.out.printf("%d x %d = %d%n", n, i, n*i);
+	}
+	
+}
+}
